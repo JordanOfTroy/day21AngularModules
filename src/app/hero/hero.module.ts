a6f8b33d63bb16e7c756fc85746800de3fe83d6a
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-13 08:22:26 -0600

    initial commit

---
+++ src/app/hero/hero.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroRoutingModule } from './hero-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    HeroDetailComponent,
+    HeroSearchComponent,
+    HeroesComponent
+  ],
+  imports: [
+    HeroRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    HeroSearchComponent
+  ]
+})
+export class HeroModule { }
